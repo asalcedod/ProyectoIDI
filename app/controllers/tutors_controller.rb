39ca92557b1138c1640bd2edb9d50e60361abc36
asalcedod <antony9409@gmail.com>
2018-04-12 18:47:21 -0500

    Monitores CRUD

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -4,13 +4,38 @@ class TutorsController < ApplicationController
 	end
 	def show
 		@tutor = Tutor.find(params[:id])
+		Tutor.where.not("id = ?", params[:id])
 	end
 	def new
 		@tutor = Tutor.new
 	end
+	def edit
+		@tutor = Tutor.find(params[:id])
+	end
 	def create
 		@tutor = Tutor.new(nombre: params[:tutor][:nombre], apellido: params[:tutor][:apellido], programa: params[:tutor][:programa], semestre: params[:tutor][:semestre], cedula: params[:tutor][:cedula], inf_contact: params[:tutor][:inf_contact])
-		@tutor.save
-		redirect_to @tutor
+		if @tutor.save
+			redirect_to @tutor
+		else
+			render :new
+		end
+	end
+	def destroy
+		@tutor = Tutor.find(params[:id])
+		@tutor.destroy
+		redirect_to tutors_path
+	end
+	def update
+		# @tutor.update_attributes({nombre: 'Nueno Nombre'})
+		@tutor = Tutor.find(params[:id])
+		if @tutor.update(tutor_params)
+			redirect_to @tutor
+		else
+			render :edit
+		end
+	end
+	private 
+	def tutor_params
+		params.require(:tutor).permit(:nombre,:apellido,:programa,:semestre,:cedula,:inf_contact)
 	end
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,2 +1,8 @@
 class Tutor < ApplicationRecord
+	validates :nombre, presence: true, length: {minimum:10 }
+	validates :apellido, presence: true, length: {minimum:10 }
+	validates :programa, presence: true, length: {minimum:10 }
+	validates :semestre, presence: true, length: {minimum:2 }
+	validates :cedula, presence: true, uniqueness: true, length: {minimum:10 }
+	validates :inf_contact, presence: true, length: {minimum:14 }
 end
