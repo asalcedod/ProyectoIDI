dd480a8e2f2d9a583e598cb80eca2d8ba2e5e2e9
asalcedod <antony9409@gmail.com>
2018-04-12 08:54:38 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'wellcome/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'wellcome#index'
+end
