caf230d71a57633112ecc2fe09ccd56033408a21
asalcedod <antony9409@gmail.com>
2018-04-13 08:41:57 -0500

    Ajustes y Fondo

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,7 +2,7 @@ class Tutor < ApplicationRecord
 	validates :nombre, presence: true, length: {minimum:10 }
 	validates :apellido, presence: true, length: {minimum:10 }
 	validates :programa, presence: true, length: {minimum:10 }
-	validates :semestre, presence: true, length: {minimum:2 }
-	validates :cedula, presence: true, uniqueness: true, length: {minimum:10 }
+	validates :semestre, presence: true, length: {minimum:1 }
+	validates :cedula, presence: true, uniqueness: true, length: {minimum:5 }
 	validates :inf_contact, presence: true, length: {minimum:14 }
 end
--- app/models/tutorium.rb
+++ app/models/tutorium.rb
@@ -1,2 +1,5 @@
 class Tutorium < ApplicationRecord
+	validates :materia, presence: true, length: {minimum:5 }
+	validates :fecha, presence: true, length: {minimum:10 }
+	validates :salon, presence: true, length: {minimum:5 }
 end
